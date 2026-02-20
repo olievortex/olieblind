f79dd5d23dbe76c685c1515da89dd5d657b22f4d
Paul Olson <olievortex@gmail.com>
2026-02-20 08:51:23 -0600

    update docs

--- scripts/mysql/olieblind_dev_backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-timestamp=$(date +%Y%m%d_%H%M%S)
-mysqldump --host=**** --port=**** --single-transaction --no-tablespaces --user=olieblind_dev_user --password=******** --set-gtid-purged=off olieblind_dev > /var/backup/mysql/${timestamp}_olieblind_dev.sql
